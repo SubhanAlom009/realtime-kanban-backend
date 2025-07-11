6095d2eaf265524b5e364dbb6d44dfa35c197b35
SubhanAlom009 <subhanalombasic123@gmail.com>
2025-07-12 00:56:40 +0530

    added JWT middleware for route protection

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+export const authUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  try {
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(401).json({
+        message: "user not found",
+      });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Authentication error:", error);
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
